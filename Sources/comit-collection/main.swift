15c0d7872f78542286e441c78c32c6060a514b96
Norman Sutorius <us.idev.student@gmail.com>
2018-02-12 22:06:31 +0100

    Add deserialization

--- Sources/comit-collection/main.swift
+++ Sources/comit-collection/main.swift
@@ -3,10 +3,21 @@ import Foundation
 let api = GitHubApiController(rootPath: "https://api.github.com/")
 let data = api.get(path: "repos/idev4u/concourse-ci-kube/contributors")
 
-do { let jsonDict = try JSONSerialization.jsonObject(with: data, options: [])
-    print("\(jsonDict)")
-} catch {
-    print(error.localizedDescription)
-}
+print("--------------------------------------------")
 
+do { let json = try? JSONSerialization.jsonObject(with: data, options: [])
+//    print(json as Any)
+    let array = json as? [Any]
+    var dict = [String:Any]()
+    
+    print("How many contributors? \(array!.count)")
+//    print("array: \(String(describing: array))")
+    for (index, item ) in array!.enumerated() {
+        dict = (item as? [String: Any])!
+        print("--------------------------------------------")
+        print("The \(index+1) Contributor of \(array!.count) has made \(dict["contributions"]!) contributions.")
+//        print(dict.debugDescription)
+        
+    }
+}
 print("done")
