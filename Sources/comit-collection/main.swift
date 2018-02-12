6003d2816e594cbeb1a94cf317ddc2ca65fd9c89
Norman Sutorius <us.idev.student@gmail.com>
2018-02-12 22:35:44 +0100

    Use login as identifier for username

--- Sources/comit-collection/main.swift
+++ Sources/comit-collection/main.swift
@@ -1,7 +1,8 @@
 import Foundation
 
+let githubUser = "idev4u"
 let api = GitHubApiController(rootPath: "https://api.github.com/")
-let data = api.get(path: "repos/idev4u/concourse-ci-kube/contributors")
+let data = api.get(path: "repos/" + githubUser + "/concourse-ci-kube/contributors")
 
 print("--------------------------------------------")
 
@@ -15,11 +16,11 @@ do { let json = try? JSONSerialization.jsonObject(with: data, options: [])
     for (index, item ) in array!.enumerated() {
         dict = (item as? [String: Any])!
         print("--------------------------------------------")
-        let userUrl = dict["html_url"] as? String
-        let username = userUrl?.split(separator: "/").last
-        print("The user: \(username!)")
-        print("The \(index+1) Contributor of \(array!.count) has made \(dict["contributions"]!) contributions.")
-//        print(dict.debugDescription)
+        let username = dict["html_url"] as? String
+        if (username?.contains(githubUser))! {
+            print("The user: \(username!)")
+            print("The \(index+1) Contributor of \(array!.count) has made \(dict["contributions"]!) contributions.")
+        }
         
     }
 }
